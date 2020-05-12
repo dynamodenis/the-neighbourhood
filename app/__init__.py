fb5aeb6b09714595b2609af00714192570369f26
Buttonupd <Dankariuki0101@gmail.com>
2020-05-12 13:24:05 +0300

    These files are developed by folder specification.Auth folder, with the view and forms file and main folder with the forms and error file.An email file has also been added

--- app/__init__.py
+++ app/__init__.py
@@ -2,19 +2,31 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_login import login_manager
+from flask_mail import Mail
+from sqlalchemy import create_engine
+
+
 
 #INSTANCES OF EXTENTIONS
+login_manager = LoginManager()
+login_manager._session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 db=SQLAlchemy()
 login_manager=LoginManager()
+mail = Mail()
 
 
 def create_app(config_name):
     app=Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
     #REGISTER EXTENTONS
-    db.init_app(app)
     login_manager.init_app(app)
-
+    mail.init_app(app)
+    db.init_app(app)
+    
     #REGISTER BLUEPRINTS
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
