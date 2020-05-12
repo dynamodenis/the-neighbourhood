3f92fafcb3cdae674fec8e47b665e1d3de4fca51
Martin Gathu <martin5gathu@gmail.com>
2020-05-12 18:55:18 +0300

    add headers to index.html file (#5)

--- app/main/views.py
+++ app/main/views.py
@@ -3,4 +3,6 @@ from flask import render_template
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date.desc()).paginate(page=page, per_page=8)
+    return render_template('index.html', posts=posts)
