b4a42033ee2a9b5ee77365846831ec9506b9b336
Dynamo Denis <dmbugua66@gmail.com>
2020-05-13 08:37:20 +0300

    created update profile form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, PasswordField, StringField, SelectField,TextAreaField
-from wtforms.validators import Required
+from wtforms.validators import Required,Email,ValidationError
+from flask_wtf.file import FileField,FileAllowed
+from ..models import User
+from flask_login import current_user
 
-class InForm(FlaskForm):
-    speak_out = TextAreaField('Is it Happening: What?')
+class PostForm(FlaskForm):
+    post = TextAreaField('Is it Happening: What?')
     title = StringField('Title')
     submit = SubmitField('Submit')
 
@@ -14,4 +17,18 @@ class Commentform(FlaskForm):
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('What is interesting about you', validators=[Required()])
-    submit = SubmitField('submit')
+    username=StringField('Type Your Username:')
+    email=StringField('Enter Your Email Address:', validators=[Email()])
+    picture=FileField('Choose a Profile Picture:', validators=[FileAllowed(['jpeg','jpg','png'])])
+    submit=SubmitField('Create Account')
+
+    def validate_username(self,data):
+        if data.data != current_user.username:
+            if User.query.filter_by(username=data.data).first():
+                raise ValidationError('This username is taken! Choose another username.')
+
+    def validate_email(self,data):
+        if data.data != current_user.email:
+            if User.query.filter_by(email=data.data).first():
+                raise ValidationError('This email is taken! Choose another email.')
+        
