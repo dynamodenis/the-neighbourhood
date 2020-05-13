f9afad38922243419dbd7315921763014bfbc4cb
Dynamo Denis <dmbugua66@gmail.com>
2020-05-13 08:37:20 +0300

    created update profile form

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 from . import  main
-from flask import render_template,redirect,request
+from flask import render_template,redirect,request,url_for,flash,abort
 from .. import db
-from ..models import Post
+from ..models import Post,User,Comment
+from .forms import Commentform,UpdateProfile,PostForm
 
 
 @main.route('/')
@@ -9,3 +10,4 @@ def index():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.posted_date.desc()).paginate(page=page, per_page=8)
     return render_template('index.html', posts=posts)
+
